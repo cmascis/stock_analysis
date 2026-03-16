9df57a2cadaf7c58b305b099daa0863aeed8fd4e
cmascis <chrismascis@outlook.com>
2026-03-15 23:03:40 -0400

    clean up to only use postgres on docker, no need for full web app on docker

--- stock_analysis/stock_analysis/settings.py
+++ stock_analysis/stock_analysis/settings.py
@@ -81,26 +81,16 @@ WSGI_APPLICATION = 'stock_analysis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DB_ENGINE = os.getenv("DB_ENGINE", "sqlite").lower()
-
-if DB_ENGINE == "postgres":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("POSTGRES_DB", "stock_analysis"),
-            "USER": os.getenv("POSTGRES_USER", "stock_analysis"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD", "stock_analysis"),
-            "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-            "PORT": os.getenv("POSTGRES_PORT", "5432"),
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "stock_analysis"),
+        "USER": os.getenv("POSTGRES_USER", "stock_analysis"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "stock_analysis"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 
 # Password validation
